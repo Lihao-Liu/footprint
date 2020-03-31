1af3f71a2c35f55f92521c646582978a8c3485c4
Lihao Liu <L_lihao@163.com>
2020-03-31 20:11:46 +0800

    Add files via upload

--- Google_translation/spider.py
+++ Google_translation/spider.py
@@ -14,6 +14,11 @@ def handle_url_method(handle_text):
     handle_url=handle_text.replace(' ','%20',999)
     # print(handle_url)
     return handle_url
+def is_contain_chinese(check_str):
+    for ch in check_str:
+        if u'\u4e00' <= ch <= u'\u9fff':
+            return True
+    return False
 def spider_translate(original_text):
     #处理输入信息中的回车
     handle_text = handle_input_test(original_text)
@@ -26,6 +31,11 @@ def spider_translate(original_text):
     r=requests.get(url='https://translate.google.cn/translate_a/single?client=webapp&sl=auto&tl=zh-CN&hl=zh-CN&dt=at&dt=bd&dt=ex&dt=ld&dt=md&dt=qca&dt=rw&dt=rm&dt=ss&dt=t&otf=2&ssel=3&tsel=0&kc=5&tk='+tk+'&q='+handle_url)
     # print('statys_code:',r.status_code)
     # print(r.text)
+    # print('*'*100)
     response=r.text.split('"')
-    # print(response)
-    return response[1],r.status_code
+    text=''
+    for i in response:
+        # print(i)
+        if is_contain_chinese(i):
+            text=text+i
+    return text,r.status_code
